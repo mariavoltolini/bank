9a9fee31b405a2717daf27f4a467928da6b92a2e
Maria <maria@ydeal.net.br>
2024-02-07 22:56:56 -0300

    first folder structures and validations

---
+++ app/Models/Movement.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'value',
+        'payer_id',
+        'receiver_id',
+    ];
+}
